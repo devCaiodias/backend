22d0bf798a9aa6a7257c31730fc973093170daba
devCaiodias <caiodiasmartins12@gmail.com>
2025-03-18 18:53:36 -0300

    arrumei a conection do meu banco de dados

--- src/db.js
+++ src/db.js
@@ -1,21 +1,15 @@
-import pg from "pg";
-import env from "dotenv"
+import pkg from "pg";
+import dotenv from "dotenv";
 
-env.config()
+dotenv.config();
 
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-})
+const { Client } = pkg;
+
+const db = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 
 db.connect();
 
-db.on('error', (err) => {
-    console.log("Unexpected error on idle client", err)
-    process.exit(-1)
-})
-
-export const query = (text, params) => db.query(text, params)
+export const query = (text, params) => db.query(text, params);
